cfe6aa37cdbadebe606ad041797953c0bb824244
Merch7x <aquamaingi@gmail.com>
2024-09-19 20:31:07 +0300

    Fetch characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,48 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-// Get the Movie ID from the first positional argument
-const movieId = process.argv[2];
+async function GetstarwarsCharacters(filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-// Check if movieId is provided
-if (!movieId) {
-  console.log('Please provide a Movie ID.');
-  process.exit(1); // Exit the script
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
 
-// Star Wars API endpoint for films
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Make a request to the Star Wars API to get the movie details
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  // Parse the response body to JSON
-  const movieData = JSON.parse(body);
-
-  // Check if movie data is valid
-  if (!movieData || !movieData.characters) {
-    console.log('Invalid Movie ID or no characters found.');
-    return;
-  }
-
-  // Get the list of character URLs
-  const characters = movieData.characters;
-
-  // For each character URL, make a request to get the character details
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        return;
-      }
-
-      // Parse the character data
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name); // Print the character's name
-    });
-  });
-});
+GetstarwarsCharacters(filmID);
